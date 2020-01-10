2779b237101124b1665fa7d955536c96fa639297
lyz <404527001@qq.com>
2020-01-10 22:35:59 +0800

    django init

--- cmsMysite/wsgi.py
+++ cmsMysite/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmsMysite.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmsMysite.settings')
+profile = os.environ.get('CMSMYSITE_PROFILE', 'dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'cmsMysite.settings.%s' % profile)
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmsMysite.settings')
+    profile = os.environ.get('CMSMYSITE_PROFILE', 'dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'cmsMysite.settings.%s' % profile)
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmsMysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
