00f362fb58c1d99b291acfe34c187daa4a547580
lyz <404527001@qq.com>
2020-01-10 22:38:02 +0800

    new splite3

---
+++ cmsMysite/settings/dev.py
@@ -0,0 +1,16 @@
+import os
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'zh-hans'
+
+TIME_ZONE = 'Asia/Shanghai'
