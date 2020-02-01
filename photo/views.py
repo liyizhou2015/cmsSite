46663b084d880dde13184399ca1fe33093a15107
lyz <404527001@qq.com>
2020-02-01 22:13:48 +0800

    time to sleep

--- photo/views.py
+++ photo/views.py
@@ -14,15 +14,25 @@ def photo_upload(request):
 
 def photo_handle(request):
 
-    f1 = request.FILES.get('pic')  # 从前端获取上传的图片   
-    fname = '{}/booktest/{}'.format(settings.MEDIA_ROOT, f1.name)  # 图片的完整路径
+    userFolder = "img/" + str(request.user.id) + "-" + \
+        str(request.user.username)
+    ffolder = '{}/{}'.format(settings.MEDIA_ROOT,
+                             userFolder)
+    if not os.path.exists(ffolder):
+        os.makedirs(ffolder)
+
+    f1 = request.FILES.get('pic')  # 从前端获取上传的图片
+    fname = ffolder + "/" + f1.name  # 图片的完整路径
     # print(fname)
+
     with open(fname, 'wb') as pic:  # 文件操作
         for c in f1.chunks():  # 因为图片存储的方式是二进制流，用f1.chunks()获取图片的字节
             pic.write(c)
+
     pic1 = Photo()
-    pic1.pic = 'booktest/%s' % f1.name
+    pic1.pic = '{}/{}'.format(userFolder, f1.name)
     pic1.author = request.user
+
     pic1.save()
     return HttpResponse('OK')
 
@@ -31,8 +41,6 @@ def photo_show(request):
     """
     相片列表 
     """
-
-    # photo_delete_all(request)
     photos = Photo.objects.all()
     context = {'photos': photos}
     return render(request, 'photo/photo_show.html', context)
